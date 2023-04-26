c6ef08e9f531b0a0ab3ef0c022315a99389d492b
muhd-umer <thisismumer@gmail.com>
2023-04-26 22:29:16 +0500

    Black formatted

---
+++ agents/dqn.py
@@ -0,0 +1,48 @@
+"""
+Implementation of DQN agent
+"""
+
+import random
+
+import gymnasium as gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from stable_baselines3.common.buffers import ReplayBuffer
+from torch.utils.tensorboard.writer import SummaryWriter
+
+seed = 0
+
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+
+
+class QNetwork(nn.Module):
+    def __init__(self, env):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Linear(np.array(env.single_observation_space.shape).prod(), 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, env.single_action_space.n),
+        )
+
+    def forward(self, x):
+        return self.network(x)
+
+
+def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
+    slope = (end_e - start_e) / duration
+
+    return max(slope * t + start_e, end_e)
+
+
+class DQNAgent(object):
+    def __init__(
+        self,
+    ):
+        pass
